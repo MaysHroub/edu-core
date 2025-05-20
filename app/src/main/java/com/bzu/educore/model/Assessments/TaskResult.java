d455b3ff9a0251e86ac4846ecfbca340c8fbf8ab
Ibrahim <ibrahimhash0594542602@gmail.com>
2025-05-20 09:59:29 +0300

    renamed assessmentType to TaskType

--- app/src/main/java/com/bzu/educore/model/Assessments/TaskResult.java
+++ app/src/main/java/com/bzu/educore/model/Assessments/TaskResult.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class AssessmentResult implements Serializable {
+public class TaskResult implements Serializable {
     private Integer assessmentId;
     private Integer studentId;
     private Double mark;
