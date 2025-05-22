6b2716f22fd144f2c5e3f758883eb3971305bc8e
Ibrahim <ibrahimhash0594542602@gmail.com>
2025-05-23 00:55:56 +0300

    commit

--- app/src/main/java/com/bzu/educore/activity/teacher/SubmittingGradesActivity.java
+++ app/src/main/java/com/bzu/educore/activity/teacher/SubmittingGradesActivity.java
@@ -10,12 +10,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.bzu.educore.R;
 
-public class AssignmentFeedBackActivity extends AppCompatActivity {
+public class SubmittingGradesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.assignment_feedback_activity);
+        setContentView(R.layout.submitting_grades_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main3), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
