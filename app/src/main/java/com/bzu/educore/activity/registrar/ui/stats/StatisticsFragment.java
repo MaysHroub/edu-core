5c2f0cdaea548ce2d5621e3e9de41d350de235cb
Mays Al-Reem <hroubmays@gmail.com>
2025-06-01 17:14:43 +0300

    feat: bind ui elements and remove greeting txt

--- app/src/main/java/com/bzu/educore/activity/registrar/ui/stats/StatisticsFragment.java
+++ app/src/main/java/com/bzu/educore/activity/registrar/ui/stats/StatisticsFragment.java
@@ -11,10 +11,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bzu.educore.databinding.FragmentStatisticsBinding;
+import com.github.mikephil.charting.charts.PieChart;
 
 public class StatisticsFragment extends Fragment {
 
     private FragmentStatisticsBinding binding;
+    private TextView txtNumOfStds, txtNumOfTeachers, txtNumOfSections, txtClassGrades;
+    private PieChart pieChartGenderDist, pieChartStdClassDist;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +28,15 @@ public class StatisticsFragment extends Fragment {
         binding = FragmentStatisticsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        TextView txtNumOfStds = binding.txtNumOfStds;
+        TextView txtNumOfTeachers = binding.txtNumOfTeachers;
+        TextView txtNumOfSections = binding.txtNumOfSections;
+        TextView txtClassGrades = binding.txtClassGrades;
+
+        PieChart pieChartGenderDist = binding.pieChartGenderDist;
+        PieChart pieChartStdClassDist = binding.pieChartStdClassDist;
+
+        // statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
