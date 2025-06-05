a861d7afb782f1462c32ad680fd1fc268b2497f7
Mays Al-Reem <hroubmays@gmail.com>
2025-06-05 18:05:07 +0300

    feat: implement a pop-up date picker

--- app/src/main/java/com/bzu/educore/activity/registrar/ui/student_registration/StudentRegistrationFragment.java
+++ app/src/main/java/com/bzu/educore/activity/registrar/ui/student_registration/StudentRegistrationFragment.java
@@ -1,20 +1,29 @@
 package com.bzu.educore.activity.registrar.ui.student_registration;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bzu.educore.databinding.FragmentStudentRegistrationBinding;
+import com.bzu.educore.util.InputValidator;
+
+import java.util.Calendar;
 
 public class StudentRegistrationFragment extends Fragment {
 
     private FragmentStudentRegistrationBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +33,39 @@ public class StudentRegistrationFragment extends Fragment {
         binding = FragmentStudentRegistrationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        studentRegistrationViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        populateFields();
+
+        binding.btnStdDob.setOnClickListener(v -> showDatePickerDialog());
+        binding.btnStdRegister.setOnClickListener(v -> addStudentToDB(studentRegistrationViewModel));
+
         return root;
+    }
+
+    private void populateFields() {
+
+    }
+
+    private void addStudentToDB(StudentRegistrationViewModel studentRegistrationViewModel) {
+
+    }
+
+    private void showDatePickerDialog() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                requireContext(),
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+                    // Month is 0-based in Calendar
+                    String date = selectedYear + "-" + (selectedMonth + 1) + "-" + selectedDay;
+                    binding.btnStdDob.setText(date);
+                },
+                year, month, day
+        );
+
+        datePickerDialog.show();
     }
 
     @Override
