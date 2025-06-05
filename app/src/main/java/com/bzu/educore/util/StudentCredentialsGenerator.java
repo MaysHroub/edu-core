666476310f022a3f48337251789687ec53fcffe5
Mays Al-Reem <hroubmays@gmail.com>
2025-06-06 01:44:06 +0300

    Âfix: make methods static

--- app/src/main/java/com/bzu/educore/util/StudentCredentialsGenerator.java
+++ app/src/main/java/com/bzu/educore/util/StudentCredentialsGenerator.java
@@ -5,14 +5,14 @@ import java.util.Date;
 
 public class StudentCredentialsGenerator {
 
-    public int generateId(int currentStudentCount) {
+    public static int generateId(int currentStudentCount) {
         String year = new SimpleDateFormat("yyyy").format(new Date());
         int nextNum = currentStudentCount + 1;
         String paddedNumber = String.format("%04d", nextNum);
         return Integer.valueOf(year + paddedNumber);
     }
 
-    public String generateEmail(int studentId) {
+    public static String generateEmail(int studentId) {
         return String.format("%d@student.educore.edu", studentId);
     }
 
