b91e0f362c3fa0deccea0d1a0c757600cab473bd
Mays Al-Reem <hroubmays@gmail.com>
2025-06-06 15:59:44 +0300

    feat: generate id and email for teacher

--- app/src/main/java/com/bzu/educore/activity/registrar/ui/teacher_registration/TeacherRegistrationFragment.java
+++ app/src/main/java/com/bzu/educore/activity/registrar/ui/teacher_registration/TeacherRegistrationFragment.java
@@ -4,28 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bzu.educore.databinding.FragmentTeacherRegistrationBinding;
+import com.bzu.educore.util.CredentialsGenerator;
+
+import java.time.LocalDate;
 
 public class TeacherRegistrationFragment extends Fragment {
 
     private FragmentTeacherRegistrationBinding binding;
+    private TeacherRegistrationViewModel teacherRegistrationViewModel;
+    private int generatedId;
+    private String generatedEmail;
+    private LocalDate dob;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        TeacherRegistrationViewModel teacherRegistrationViewModel =
+        teacherRegistrationViewModel =
                 new ViewModelProvider(this).get(TeacherRegistrationViewModel.class);
 
         binding = FragmentTeacherRegistrationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        teacherRegistrationViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        teacherRegistrationViewModel.getNumOfTeachers().observe(getViewLifecycleOwner(), numOfTeachers -> {
+            generatedId = CredentialsGenerator.generateTeacherId(numOfTeachers);
+            generatedEmail = CredentialsGenerator.generateTeacherEmail(generatedId);
+            binding.edttxtTchrId.setText(generatedId+"");
+            binding.edttxtTchrEmail.setText(generatedEmail);
+        });
+
         return root;
     }
 
