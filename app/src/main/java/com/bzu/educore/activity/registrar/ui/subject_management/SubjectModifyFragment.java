83f4dcfc8d9bb030e180501226f676647cc0fa07
Mays Al-Reem <hroubmays@gmail.com>
2025-06-07 02:20:37 +0300

    ui: create ui for subject-modifying fragment

---
+++ app/src/main/java/com/bzu/educore/activity/registrar/ui/subject_management/SubjectModifyFragment.java
@@ -0,0 +1,22 @@
+package com.bzu.educore.activity.registrar.ui.subject_management;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bzu.educore.R;
+
+public class SubjectModifyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_subject_modify, container, false);
+    }
+}
