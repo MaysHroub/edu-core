f7568d82bf92d7ae8ee882ec0a8ce7a1d5309944
Mays Al-Reem <hroubmays@gmail.com>
2025-06-07 15:11:48 +0300

    ui: create ui for view-all-users function

---
+++ app/src/main/java/com/bzu/educore/activity/registrar/ui/student_management/ViewAllStudentsFragment.java
@@ -0,0 +1,22 @@
+package com.bzu.educore.activity.registrar.ui.student_management;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bzu.educore.R;
+
+public class ViewAllStudentsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_all_students, container, false);
+    }
+
+}
