14b8a03dcffd90236d364a9fcac352460575e396
Mays Al-Reem <hroubmays@gmail.com>
2025-06-09 19:25:22 +0300

    fix: use regular fragment for nav_host_fragment

--- app/src/main/java/com/bzu/educore/activity/registrar/RegistrarMainActivity.java
+++ app/src/main/java/com/bzu/educore/activity/registrar/RegistrarMainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -35,9 +36,11 @@ public class RegistrarMainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_teachers, R.id.nav_students, R.id.nav_subjects, R.id.nav_classrooms, R.id.nav_schedules, R.id.nav_profile)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_registrar_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_registrar_main);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(navigationView, navController);
+        }
     }
 
     @Override
